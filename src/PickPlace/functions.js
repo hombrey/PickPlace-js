d94ba53789313c615e0610095fe3d8619d7e1761
Hombrey Escalante <hombrey.escalante@gmail.com>
2021-06-12 20:06:14 +0700

    100% working

--- src/PickPlace/functions.js
+++ src/PickPlace/functions.js
@@ -6,6 +6,7 @@ let scaleX, scaleY;
 let piece1;
 let pieces;
 let pickedNum=1;
+let pickSound;
 ;//}}}variable declarations
 
 //{{{event listeners
@@ -13,35 +14,42 @@ window.onload = initWin();
 window.addEventListener("resize", initWin);
 window.addEventListener("keyup", evalKeyUp, false); //capture keypress on bubbling (false) phase
 window.addEventListener("keydown", evalKeyDown, false); //capture keypress on bubbling (false) phase
-window.addEventListener("mousedown", movePiece, true); //capture keypress on bubbling (false) phase
-window.addEventListener("mouseup", leavePiece, false); //capture keypress on bubbling (false) phase
+/*window.addEventListener("mousedown", movePiece, true); //capture keypress on bubbling (false) phase*/
+//window.addEventListener("mouseup", leavePiece, false); //capture keypress on bubbling (false) phase
+function evalPress(evnt) {
+    let keyPressed = evnt.keyCode;
+    //console.log ("Pressed:", keyPressed);
+} //function evalPress
 //window.addEventListener("mousemove",followMouse);
 function evalKeyDown(evnt) {
     let keyPressed = evnt.keyCode;
-    if (keyPressed==16) movePiece(); //key: SHIFT -- drag selected piece
-    if (keyPressed==81) selectPiece(1); //key: q
-    if (keyPressed==87) selectPiece(2); //key: w
-    if (keyPressed==69) selectPiece(3); //key: e
-    if (keyPressed==82) selectPiece(4); //key: r
-    if (keyPressed==84) selectPiece(5); //key: t
-    if (keyPressed==89) selectPiece(6); //key: y
-    if (keyPressed==49) placePiece(1); //key: 1
-    if (keyPressed==50) placePiece(2); //key: 2
-    if (keyPressed==51) placePiece(3); //key: 3
-    if (keyPressed==52) placePiece(4); //key: 4
-    if (keyPressed==53) placePiece(5); //key: 5
-    if (keyPressed==54) placePiece(6); //key: 6
+    if (keyPressed==32) movePiece(); //key: spacebar -- drag selected piece
+    if(!event.shiftKey) if (keyPressed==49) selectPiece(1); //key: 1
+    if(!event.shiftKey) if (keyPressed==50) selectPiece(2); //key: 2
+    if(!event.shiftKey) if (keyPressed==51) selectPiece(3); //key: 3
+    if(!event.shiftKey) if (keyPressed==52) selectPiece(4); //key: 4
+    if(!event.shiftKey) if (keyPressed==53) selectPiece(5); //key: 5
+    if(!event.shiftKey) if (keyPressed==54) selectPiece(6); //key: 6
+    if(event.shiftKey) if (keyPressed==49) placePiece(1); //key: shift-1
+    if(event.shiftKey) if (keyPressed==50) placePiece(2); //key: shift-2
+    if(event.shiftKey) if (keyPressed==51) placePiece(3); //key: shift-3
+    if(event.shiftKey) if (keyPressed==52) placePiece(4); //key: shift-4
+    if(event.shiftKey) if (keyPressed==53) placePiece(5); //key: shift-5
+    if(event.shiftKey) if (keyPressed==54) placePiece(6); //key: shift-6
 } //evalKey(event)
 function evalKeyUp(evnt) {
     let keyPressed = evnt.keyCode;
-    console.log ("keyUp: ",keyPressed);
-    if (keyPressed==81) resetPiece(1); //key: q
-    if (keyPressed==87) resetPiece(2); //key: w
-    if (keyPressed==69) resetPiece(3); //key: e
-    if (keyPressed==82) resetPiece(4); //key: r
-    if (keyPressed==84) resetPiece(5); //key: t
-    if (keyPressed==89) resetPiece(6); //key: y
-    if (keyPressed==16) leavePiece(); //key: Shift
+    //console.log ("keyUp: ",keyPressed);
+    if(!event.shiftKey) if (keyPressed==49) resetPiece(1); //key: 1
+    if(!event.shiftKey) if (keyPressed==50) resetPiece(2); //key: 2
+    if(!event.shiftKey) if (keyPressed==51) resetPiece(3); //key: 3
+    if(!event.shiftKey) if (keyPressed==52) resetPiece(4); //key: 4
+    if(!event.shiftKey) if (keyPressed==53) resetPiece(5); //key: 5
+    if(!event.shiftKey) if (keyPressed==54) resetPiece(6); //key: 6
+    if (keyPressed==32) leavePiece(); //key: spacebar
+    if(!event.shiftKey) if (keyPressed==188) toggleHide();
+    if(event.shiftKey) if (keyPressed==188) hideAll();
+    if (keyPressed==190) playPrompt();
 } //evalKey(event)
 //}}}event listeners
 
@@ -54,21 +62,27 @@ function placeLocations() {
 
     pieces[1-1].pickX = 1026;
     pieces[1-1].pickY = 426;
+    pieces[1-1].prompt = new sound("./src/PickPlace/wav/prompt1.mp3");
 
     pieces[2-1].pickX = 30;
     pieces[2-1].pickY = 441;
+    pieces[2-1].prompt = new sound("./src/PickPlace/wav/prompt2.mp3");
 
     pieces[3-1].pickX = 10;
     pieces[3-1].pickY = 31;
+    pieces[3-1].prompt = new sound("./src/PickPlace/wav/prompt3.mp3");
 
     pieces[4-1].pickX = 736;
     pieces[4-1].pickY = 482;
+    pieces[4-1].prompt = new sound("./src/PickPlace/wav/prompt4.mp3");
 
     pieces[5-1].pickX = 365;
     pieces[5-1].pickY = 475;
+    pieces[5-1].prompt = new sound("./src/PickPlace/wav/prompt5.mp3");
 
     pieces[6-1].pickX = 1019;
     pieces[6-1].pickY = 12;
+    pieces[6-1].prompt = new sound("./src/PickPlace/wav/prompt6.mp3");
 
 
     pieces[1-1].placeX = 349;
@@ -88,6 +102,8 @@ function placeLocations() {
 
     pieces[6-1].placeX = 703;
     pieces[6-1].placeY = 237;
+
+    pickSound = new sound("./src/PickPlace/wav/pick.mp3");
 } //function placePieces
 function initWin() {
     //Get a reference to the canvas
@@ -98,20 +114,21 @@ function initWin() {
     scaleX = bgX.clientWidth/bgX.naturalWidth;
     scaleY = bgX.clientHeight/bgX.naturalHeight;
     //console.log ("scale: ("+scaleX+","+scaleY+")");
+
     placeLocations();
     for (let pInx=1; pInx<pieces.length+1; pInx++) {
-        console.log ("pInx: ",pInx);
+        //console.log ("pInx: ",pInx);
         pieces[pInx-1].Width = Math.round (scaleX*pieces[pInx-1].naturalWidth);
         pieces[pInx-1].Height = Math.round (scaleY*pieces[pInx-1].naturalHeight);
         insertCss ("#piece"+pInx+" {width: "+ pieces[pInx-1].Width +"px; height: "+ pieces[pInx-1].Height +"px;}");
 
-        //pieces[pInx-1].X = Math.round (scaleX*pieces[pInx-1].placeX);
-        //pieces[pInx-1].Y = Math.round (scaleY*pieces[pInx-1].placeY);
         pieces[pInx-1].X = Math.round (scaleX*pieces[pInx-1].pickX);
         pieces[pInx-1].Y = Math.round (scaleY*pieces[pInx-1].pickY);
         
         insertCss ("#piece"+pInx+"{left: "+ pieces[pInx-1].X +"px; top: "+ pieces[pInx-1].Y +"px;}");
-        console.log ("#piece"+pInx+"{left: "+ pieces[pInx-1].X +"px; top: "+ pieces[pInx-1].Y +"px;}");
+
+        pieces[pInx-1].show = true;
+        //console.log ("#piece"+pInx+"{left: "+ pieces[pInx-1].X +"px; top: "+ pieces[pInx-1].Y +"px;}");
     } //for (pInx=1; pInx=pieces.size+1; pInx+)
     
     //console.log ("bgX CSS-height: "+ bgXstyle.height);
@@ -125,6 +142,12 @@ function initWin() {
 //}}}window init
 
 //{{{handler functions
+function clickPiece(clicked_id) {
+    let extractIdNum = (clicked_id.replace("piece",""));
+    let clickedNum = parseInt(extractIdNum);
+    pickedNum = clickedNum;
+    pickSound.play();
+} //function clickPiece(clicked_id)
 function selectPiece(numPassed) {
     pickedNum=numPassed;
     window.addEventListener("mousemove",followMouse);
@@ -133,8 +156,10 @@ function selectPiece(numPassed) {
 function placePiece(numPassed) {
     pieces[pickedNum-1].style.left = Math.round (scaleX*pieces[numPassed-1].placeX)+'px';
     pieces[pickedNum-1].style.top = Math.round (scaleY*pieces[numPassed-1].placeY)+'px';
+    pickSound.play();
 } //function selectPiece(pieceNum)
 function resetPiece() {
+    pickSound.play();
     window.removeEventListener("mousemove",followMouse);
     insertCss (".pieceClass {transition: 100ms;}"); 
     pieces[pickedNum-1].style.left = Math.round (scaleX*pieces[pickedNum-1].pickX)+'px';
@@ -146,17 +171,35 @@ function movePiece() {
     console.log("picked", pickedNum); 
 } //function leavePiece()
 function leavePiece() {
+    pickSound.play();
     window.removeEventListener("mousemove",followMouse);
     insertCss (".pieceClass {transition: 100ms;}"); 
 } //function leavePiece()
 function followMouse() {
-    
     let x = event.clientX;
     let y = event.clientY;
     pieces[pickedNum-1].style.left = x+'px';
     pieces[pickedNum-1].style.top = y+'px';
 
 } //function followMouse()
+function playPrompt() {
+    pieces[pickedNum-1].prompt.play();
+} //function playPrompt()
+function toggleHide() {
+    if (pieces[pickedNum-1].show) {
+        pieces[pickedNum-1].src ="./src/PickPlace/img/"+pickedNum+"h.jpg";
+        pieces[pickedNum-1].show=false;
+    } else {//if (pieces[pickedNum-1].show)
+        pieces[pickedNum-1].src ="./src/PickPlace/img/"+pickedNum+".png";
+        pieces[pickedNum-1].show=true;
+    } // else //if (pieces[pickedNum-1 ...
+} //function toggleHide()
+function hideAll() {
+    for (let pInx=1; pInx<pieces.length+1; pInx++) {
+        pieces[pInx-1].src ="./src/PickPlace/img/"+pInx+"h.jpg";
+        pieces[pInx-1].show = false;
+    } //for (pInx=1; pInx=pieces.size+1; pInx+)
+} //function hideAll()
 
 //}}}handler functions
 
